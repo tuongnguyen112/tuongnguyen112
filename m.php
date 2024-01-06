602f165cf7132e797b93493a13124514a33a5905
tuongnguyen112 <155818181+tuongnguyen112@users.noreply.github.com>
2024-01-06 18:52:04 +0700

    Update m.php

--- m.php
+++ m.php
@@ -1,14 +1,1 @@
-
-$red="\033[1;31m";
-$green="\033[1;32m";
-$yellow="\033[1;33m";
-$blud="\033[1;34m";
-$res="\033[1;35m";
-$nau="\033[1;36m";
-$trang="\033[1;37m";
-error_reporting(0);
-system("clear");
-
-xoa het
-
-
+tuong nguyen
